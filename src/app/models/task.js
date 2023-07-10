95c3325f52317e508f1f0d2ff3b629cecc59ab82
anhtoan260497 <anhtoan260497@gmail.com>
2023-07-10 17:54:20 +0700

    create task and project queries

--- src/app/models/task.js
+++ src/app/models/task.js
@@ -4,44 +4,59 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema(
   {
     email: {
-      type : String,
-      require : true,
+      type: String,
+      require: true,
     },
-    tasks: [    
+    projects: [
       {
         title: {
-          type  : String,
-          required : true
+          type: String,
+          required: true,
         },
-        description: String,
-        date : {
-          type : Number,
-          required : true
-        }, 
-        subTask: [
+        tasks: [
           {
-            label: String,
-            isDone: Boolean,
-          },
-        ],
-        comments: [
-          {
-            label: {
-              type : String,
-              required : true
+            title: {
+              type: String,
+              required: true,
             },
-            timestamps: {
+            description: String,
+            date: {
+              type: Number,
+              required: true,
+            },
+            subTask: [
+              {
+                label: String,
+                isDone: Boolean,
+              },
+            ],
+            comments: [
+              {
+                label: {
+                  type: String,
+                  required: true,
+                },
+                timestamps: {
+                  type: Number,
+                  required: true,
+                },
+                description: {
+                  type: String,
+                  required: true,
+                },
+              },
+            ],
+            priority: {
               type : Number,
               required : true
             },
-            description: {
+            labels: [],
+            project: {
               type : String,
               required : true
             },
           },
         ],
-        priority: Number,
-        labels: [],
       },
     ],
   },
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const authRouters = require("./auth");
 const taskRouters = require("./task");
+const projectRouters = require("./project");
 
 const routes = (app) => {
-  app.use("/api/auth",authRouters);
+  app.use("/api/auth", authRouters);
   app.use("/api/task", taskRouters);
+  app.use("/api/project", projectRouters);
 };
 
 module.exports = routes;
