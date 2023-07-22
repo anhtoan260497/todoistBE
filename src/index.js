56447ebce52e132f05323530ed5874950ca54ba7
anhtoan260497 <anhtoan260497@gmail.com>
2023-07-23 01:08:28 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const connectDB = require("./config/database/connectDB");
 const routes = require("./routes");
 const cookies =  require('cookie-parser');
-const  cors = require('cors')
+// const  cors = require('cors')
 
 
 // db connect
@@ -17,7 +17,18 @@ const port = 8080;
 
 //CORS Enable
 
-app.use(cors())
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://todoist-ten-ebon.vercel.app', 'https://todoist-ten-ebon.vercel.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 
 
